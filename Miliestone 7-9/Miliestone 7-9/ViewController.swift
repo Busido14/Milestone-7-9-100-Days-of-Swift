cfe1ee24ab062d59a7a96eeec4d137c2176df59d
Artem <artchzhen@gmail.com>
2023-04-15 18:17:18 +0900

    initial commit devolopment

---
+++ Miliestone 7-9/Miliestone 7-9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Miliestone 7-9
+//
+//  Created by Артем Чжен on 15/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
